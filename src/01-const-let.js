5cf94ccf5042c6b02f756e16de4320b09fbcba74
Victor Ramirez <vramirez@utj.edu.mx>
2023-09-20 18:35:53 -0600

    Practica 1 terminada

---
+++ src/01-const-let.js
@@ -0,0 +1,12 @@
+
+const nombre = 'Victor';
+const apellido = 'Ramirez';
+
+let valorDado = 5;
+
+if( true ){
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(nombre, apellido, valorDado);
